569779d298dfc39957ce1562453b4900bb22fdc4
Xinke Chen <xinkecf35@gmail.com>
2017-05-03 22:18:25 -0400

    Added scripting for navigation

---
+++ scrolltolink.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $("nav ul li a").click(function(event) {
+        event.preventDefault();
+        var requestedDiv = $(this).attr('href');
+        var scrollToDiv = $(requestedDiv).offset().top;
+        $('html, body').animate({scrollTop: scrollToDiv},1500, "easeOutCubic");
+    });
+});
--- sticknav.js
+++ sticknav.js
@@ -2,13 +2,12 @@ $(document).ready(function() {
     var nav = $("nav");
     var about = $("#about");
     var height = about.offset().top;
-    console.log(height);
     $(window).scroll(function (event) {
-        if($(window).scrollTop() > height) {
-            nav.addClass("fix-nav",1500);
+        if($(window).scrollTop() + 5 > height) {
+            nav.addClass("fix-nav");
         }
         else{
-            nav.removeClass("fix-nav",1500);
+            nav.removeClass("fix-nav");
         }
     });
 });
